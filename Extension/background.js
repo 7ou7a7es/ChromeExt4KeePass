719df30cf325db307ab57d9f69ac05e422633686
Guillaume Jourdan <5509339+gujou@users.noreply.github.com>
2021-01-06 12:00:41 +0100

    update with domain name (& tls)

--- Extension/background.js
+++ Extension/background.js
@@ -1,7 +1,20 @@
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-		if (tab.title != tab.url){
-			chrome.tabs.sendMessage(tabId, {command: tab.url});
+		var splitUrl = tab.url.split("/",3);
+		var msg = splitUrl[2];
+		
+		if (msg!=null && msg.includes("www.")){
+			msg = msg.replace("www.","");
+		}
+		
+		if (msg!=null && splitUrl[0].includes("https")){
+			msg = msg + " (tls)"
+		}
+		
+		if (msg!=null && tab.title != msg){
+			window.alert("tilte : " + tab.title)
+			window.alert("msg : "+msg);
+			chrome.tabs.sendMessage(tabId, {command: msg});
 		}
 	});
 })

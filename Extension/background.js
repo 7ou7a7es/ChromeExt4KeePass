cec2f3891d3ca3b6067019e1b209cb5c2b28fc1d
Guillaume Jourdan <5509339+gujou@users.noreply.github.com>
2021-01-08 08:44:50 +0100

    fixe NPE

--- Extension/background.js
+++ Extension/background.js
@@ -1,20 +1,28 @@
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-		var splitUrl = tab.url.split("/",3);
-		var msg = splitUrl[2];
+		var splitUrl;
+		var msg;
 		
-		if (msg!=null && msg.includes("www.")){
-			msg = msg.replace("www.","");
-		}
-		
-		if (msg!=null && splitUrl[0].includes("https")){
-			msg = msg + " (tls)"
-		}
-		
-		if (msg!=null && tab.title != msg){
-			console.log("title : " + tab.title)
-			console.log("msg : " + msg);
-			chrome.tabs.sendMessage(tabId, {command: msg, action: "title"});
+		if (tab.url != null){
+			splitUrl = tab.url.split("/",3);
+			
+			if (splitUrl.length > 2){
+				msg = splitUrl[2];
+			}
+
+			if (msg!=null && msg.includes("www.")){
+				msg = msg.replace("www.","");
+			}
+
+			if (msg!=null && splitUrl[0].includes("https")){
+				msg = msg + " (tls)"
+			}
+
+			if (msg!=null && tab.title != msg){
+				console.log("title : " + tab.title)
+				console.log("msg : " + msg);
+				chrome.tabs.sendMessage(tabId, {command: msg, action: "title"});
+			}
 		}
 	});
 })
